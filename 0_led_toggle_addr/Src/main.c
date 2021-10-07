01f8bd6fe5adc03cfc7ccba02a6722575a31e144
Debugging Duck <isabella.ragona@live.it>
2021-10-07 17:48:49 +0200

    [0_led_toggle_addr] conditional compilation to select HW target. Application is more portable

--- 0_led_toggle_addr/Src/main.c
+++ 0_led_toggle_addr/Src/main.c
@@ -1,20 +1,11 @@
-#include "STM32F401RETx.h"
+#include "BSP.h"
 
 int main (void)
 {
-	// 1. Enable clock access to GPIOA
-	RCC->AHB1ENR |= GPIOAEN;
+	LedInit ();
 
-	// 2. Set PA5 as output pin
-	
-	GPIOA->MODER |= (1U << 10); // Set bit 10 to 1
-	GPIOA->MODER &=~(1U << 11); // Set bit 11 to 0
+	LedOn ();
+	LedOff ();
 
-	for (int i = 0; i < 11; i++)
-	{
-		// 4. Set PA5 high
-		// GPIOA_OD_R |= LED_PIN;
-		// 5. toggle PA5
-		GPIOA->ODR ^= LED_PIN;
-	}
+	LedToggle ();
 }
