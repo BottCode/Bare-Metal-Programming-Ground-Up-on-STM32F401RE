9bafa68ee698b84e5ffb3d53d83782108719a2a6
Mattia Bottaro <mattiabottaro9@gmail.com>
2022-01-04 21:24:07 +0100

    Updated compilation script 0_led

--- exercises/0_led_toggle_addr/compile_and_flash.sh
+++ exercises/0_led_toggle_addr/compile_and_flash.sh
@@ -1,1 +1,3 @@
-cd ../../ && source ./set_env_var.sh && cd $EXERCISES_PATH/0_led_toggle_addr/ && source ./set_env_var.sh && cd Debug && make clean && make && st-flash write 0_led_toggle_addr.bin 0x08000000 && cd ..
+cd ../../ && source ./set_env_var.sh && cd $EXERCISES_PATH/0_led_toggle_addr/ \
+&& source ./set_env_var.sh && cd Debug && make clean \
+&& make && st-flash write 0_led_toggle_addr.bin 0x08000000 && cd ..
