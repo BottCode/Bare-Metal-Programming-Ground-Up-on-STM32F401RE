79dd37420b1344b1d28e70c176d974c8ac9f5f3c
Mattia Bottaro <mattiabottaro9@gmail.com>
2022-01-04 23:26:17 +0100

    [Led_tog_original] fixed build script

--- exercises/led_toggle_original_driver/compile_and_flash.sh
+++ exercises/led_toggle_original_driver/compile_and_flash.sh
@@ -1,4 +1,4 @@
 cd ../../ && source ./get_submodule.sh && \
 source ./set_env_var.sh && cd $EXERCISES_PATH/led_toggle_original_driver/ && \
 source ./set_env_var.sh && cd Debug && make clean && make && \
-st-flash write 0_led_toggle_addr.bin 0x08000000 && cd ..
+st-flash write exercise.bin 0x08000000 && cd ..
