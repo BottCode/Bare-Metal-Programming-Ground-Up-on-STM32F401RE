7302a70e893f54af8f218151df298764e281fbd9
Mattia Bottaro <mattiabottaro9@gmail.com>
2022-01-07 16:07:35 +0100

    [UART] init UART as RX and TX

--- ST_Driver_Facade/IO.h
+++ ST_Driver_Facade/IO.h
@@ -5,8 +5,10 @@
 #define LED_PIN     	(PIN5)
 
 #define CR1_TE 			(1U << 3)
+#define CR1_RE 			(1U << 2)
 #define CR1_UE 			(1U << 13)
 #define SR_TXE 			(1U << 7)
+#define SR_RXNE 			(1U << 5)
 
 #define SYS_FREQ		16000000
 #define APB1_CLK		SYS_FREQ
@@ -76,6 +78,7 @@ void uart2_tx_init (void)
 	// enable uart module
 	USART2->CR1 |= CR1_UE;
 }
+
 void uart2_write (int ch)
 {
 	// Make sure the trasmit data register is empty
@@ -96,4 +99,57 @@ static void uart_set_baudrate (USART_TypeDef * USARTx, uint32_t PeriphClk, uint3
 static uint16_t compute_uart_bd (uint32_t PeriphClk, uint32_t BaudRate)
 {
 	return ((PeriphClk + (BaudRate / 2U)) / BaudRate);
+}
+
+/* Initialize both RX and TX functionalities */
+void uart2_rxtx_init (void)
+{
+	/* configure uart gpio pin */
+	// enable clock access to gpioa
+	RCC->AHB1ENR |= GPIOAEN;
+
+	// Set PA2 to alternate function mode
+	GPIOA->MODER &= ~(1U << 4);
+	GPIOA->MODER |= (1U << 5);
+
+	// Set PA2 alternate function type to UART_TX (AF07)
+	GPIOA->AFR[0] |= (1U << 8);
+	GPIOA->AFR[0] |= (1U << 9);
+	GPIOA->AFR[0] |= (1U << 10);
+	GPIOA->AFR[0] &= ~(1U << 11);
+
+	// Set PA3 alternate function mode
+	GPIOA->MODER &= ~(1U << 6);
+	GPIOA->MODER |= (1U << 7);
+
+	// Set PA3 alternate function type to UART_RX (AF07)
+	GPIOA->AFR[0] |= (1U << 12);
+	GPIOA->AFR[0] |= (1U << 13);
+	GPIOA->AFR[0] |= (1U << 14);
+	GPIOA->AFR[0] &= ~(1U << 15);
+
+	/* configure uart module */
+	// enable clock access to uart2
+	RCC->APB1ENR |= UART2EN;
+
+	// configure baudrate
+	uart_set_baudrate (USART2, APB1_CLK, UART_BAUDRATE);
+
+	// configure the transfer direction. No OR operator: we want to clean the whole CR1 register.
+	USART2->CR1 = (CR1_TE | CR1_RE);
+
+	// enable uart module
+	USART2->CR1 |= CR1_UE;
+}
+
+char uart2_read (void)
+{
+	// Make sure the receive data register is NOT empty
+	while (!(USART2->SR & SR_RXNE))
+	{
+		;// Make sure the trasmit data register is empty
+	}
+
+	// Read data
+	return USART2->DR;
 }
